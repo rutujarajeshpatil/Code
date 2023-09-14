0166f574a1327fb92b7b978c12c12b75eaab0da8
Rutuja Patil <144714173+rutujarajeshpatil@users.noreply.github.com>
2023-09-14 23:55:47 +0530

    Add files via upload

---
+++ 60-area and perimeter.py
@@ -0,0 +1,15 @@
+def area(a,b):
+    c=a*b
+    return c
+a=int(input("Enter value a:"))
+b=int(input("Enter value b:"))
+c=area(a,b)
+print("area is:",c)
+
+def perimeter(a,b):
+    d=2*(a+b)
+    return c
+a=int(input("Enter value a:"))
+b=int(input("Enter value b:"))
+d=perimeter(a,b)
+print("perimeter is:",d)
